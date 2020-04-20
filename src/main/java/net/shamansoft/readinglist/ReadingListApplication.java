affb2e061899845ea9c16d7c9084c66f16e81d37
Alexey Khisamutdinov <alexey-khisamutdinov@users.noreply.github.com>
2020-04-20 13:49:24 -0700

    add client for books server

--- src/main/java/net/shamansoft/readinglist/ReadingListApplication.java
+++ src/main/java/net/shamansoft/readinglist/ReadingListApplication.java
@@ -1,20 +1,31 @@
 package net.shamansoft.readinglist;
 
+import net.shamansoft.readinglist.rest.client.BooksClient;
+import net.shamansoft.readinglist.rest.dto.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.Console;
+import java.util.List;
 
 @SpringBootApplication
 public class ReadingListApplication implements ApplicationRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(ReadingListApplication.class);
 	private final HelloService helloService;
+	private final BooksClient booksClient;
 
-	public ReadingListApplication(HelloService helloService){
+	public ReadingListApplication(HelloService helloService,
+								  BooksClient booksClient){
 		this.helloService = helloService;
+		this.booksClient = booksClient;
 	}
 
 	public static void main(String[] args) {
@@ -22,15 +33,17 @@ public class ReadingListApplication implements ApplicationRunner {
 	}
 
 	@Override
-	public void run(ApplicationArguments args) {
-//		log.info("*nonargs");
-//		args.getNonOptionArgs().forEach(log::info);
-//		log.info("*names");
-//		args.getOptionNames().forEach(log::info);
-//		log.info("*vals");
-//		args.getOptionNames().forEach(option -> log.info(String.format("%s = %s", option, args.getOptionValues(option).toString())));
-
-		log.info(helloService.sayHello(args.getOptionValues("name")));
-
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println(helloService.sayHello(args.getOptionValues("name")));
+		List<Book> bookList = booksClient.findAll();
+		bookList.forEach(book -> log.info(book.toString()));
+		Console console = System.console();
+		if (console == null) {
+			System.out.println("No console: non-interactive mode!");
+			return;
+		}
+		System.out.println("How do you feel?");
+		String iFeel = console.readLine();
+		System.out.println(iFeel.contains("good") ? "I'm glad to hear this!" : "Sorry to hear this.");
 	}
 }
