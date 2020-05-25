2923f48af14fa35735a1b9ceec6fa7ff89997679
Alexey Khisamutdinov <alexey-khisamutdinov@users.noreply.github.com>
2020-05-24 22:06:03 -0700

    clenup imports

--- reading-list/src/main/java/net/shamansoft/readinglist/ReadingListApplication.java
+++ reading-list/src/main/java/net/shamansoft/readinglist/ReadingListApplication.java
@@ -8,9 +8,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.Console;
 import java.util.List;
